37d9cf002863751f9ceeb3ae1414d1d8f59da858
Moringa School <maryanneanyango98@gmail.com>
2022-08-21 15:08:08 +0300

    commit tree 13d36bf26ef31f5844a6a13f2687a37cc69d8612

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    if array.length >= 3 
+      arr= array.delete_at(array.length-1)
+      return ("#{array.join(', ')}, and #{arr}" )
+  elsif array.length == 2
+      return array.join(" and ")
+  elsif array.length == 1
+      return array.join    
+  end 
+  end
